99053226470c6f0d7611916376ed045185def4cd
CongYe <970266313@qq.com>
2025-10-23 17:49:52 +0800

    Refactor routes to use specific domain names
    Updated routes for production and staging with specific domains.

--- src/index.js
+++ src/index.js
@@ -6,18 +6,13 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
-
-  // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  "docker.mydomain.com": "https://registry-1.docker.io",
+  "quay.mydomain.com": "https://quay.io",
+  "gcr.mydomain.com": "https://gcr.io",
+  "k8s-gcr.mydomain.com": "https://k8s.gcr.io",
+  "k8s.mydomain.com": "https://registry.k8s.io",
+  "ghcr.mydomain.com": "https://ghcr.io",
+  "cloudsmith.mydomain.com": "https://docker.cloudsmith.io"
 };
 
 function routeByHosts(host) {
